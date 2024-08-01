155c427ff688896db54fe413b2d383011cf8fa38
KKrishna11 <kushwaha.krishna1233@gmail.com>
2024-08-01 12:45:17 +0530

    changes in files

---
+++ script.js
@@ -0,0 +1,20 @@
+var component1Container = document.getElementById("component1Container");
+if (component1Container) {
+  component1Container.addEventListener("click", function (e) {
+    window.location.href = "./panel1.html";
+  });
+}
+
+var panel16Container = document.getElementById("panel16Container");
+if (panel16Container) {
+  panel16Container.addEventListener("click", function (e) {
+    window.location.href = "./panel1.html";
+  });
+}
+
+// var eiplus = document.getElementById("eiplus");
+// if (eiplus) {
+//   eiplus.addEventListener("click", function (e) {
+//     window.location.href = "./panel2.html";
+//   });
+// }
